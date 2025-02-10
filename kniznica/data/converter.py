96bb1b02394f4da616c059fd47f9119ef5e947c2
j-matuska <jan.matuska@stuba.sk>
2025-02-10 22:18:55 +0100

    parallel converter

--- kniznica/data/converter.py
+++ kniznica/data/converter.py
@@ -41,30 +41,30 @@ class AtomsConverterModule:
         
     def __call__(self, inputs):
         batch_size = len(inputs)//self.n_cpu
-        # pool = multiprocessing.Pool(processes=self.n_cpu)
-        # print(inputs)
-        # print(type(inputs))
-        # outputs = pool.map(self.converter, inputs, chunksize=batch_size)
-        # pool.close()
-        # pool.join()
-        processes = []
-        outputs = multiprocessing.Manager().dict()
-        for i in range(self.n_cpu+1):
-            p = multiprocessing.Process(
-                target=self.converter2, 
-                args=(
-                    i,
-                    inputs[i*batch_size:(i+1)*batch_size],
-                    outputs)
-                )
-            processes.append(p)
-            p.start()
-        for p in processes:
-            p.join()
-        #print(outputs)
-        c_outputs = []
-        for i in range(self.n_cpu+1):
-            c_outputs.extend(outputs[i])
+        pool = multiprocessing.Pool(processes=self.n_cpu)
+        #print(inputs)
+        #print(type(inputs))
+        c_outputs = pool.map(self.converter, inputs)#, chunksize=batch_size)
+        pool.close()
+        pool.join()
+        # processes = []
+        # outputs = multiprocessing.Manager().dict()
+        # for i in range(self.n_cpu+1):
+        #     p = multiprocessing.Process(
+        #         target=self.converter2, 
+        #         args=(
+        #             i,
+        #             inputs[i*batch_size:(i+1)*batch_size],
+        #             outputs)
+        #         )
+        #     processes.append(p)
+        #     p.start()
+        # for p in processes:
+        #     p.join()
+        # #print(outputs)
+        # c_outputs = []
+        # for i in range(self.n_cpu+1):
+        #     c_outputs.extend(outputs[i])
         # c_outputs = outputs[0]    
         # for i in range(1,self.n_cpu+1):
         #     for key in outputs[0].keys():
